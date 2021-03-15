7392cdc41bce5259f0b6230ac53eceb99a84815f
Alperen <aalperenelbasan@gmail.com>
2021-03-14 23:02:18 -0500

    blocks crud actions are done

--- src/main/java/com/mumSchud/springbootmum/entity/Block.java
+++ src/main/java/com/mumSchud/springbootmum/entity/Block.java
@@ -9,43 +9,41 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "courses")
-public class Course {
+@Table(name = "blocks")
+public class Block {
 	@Id
 	@GeneratedValue
 	private int id;
 	
 	private String name;
-	private String code;
+	private String date;
 	
-	public Course() {
+	public Block() {
 		
 	}
 	
-	public Course(String name, String code) {
+	public Block(String name, String date) {
 		super();
 		this.name = name;
-		this.code = code;
+		this.date = date;
 	}
-
-
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getCode() {
-		return code;
+	public String getDate() {
+		return date;
 	}
-	public void setCode(String code) {
-		this.code = code;
+	public void setDate(String date) {
+		this.date = date;
 	}
 }
