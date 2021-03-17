3d1ac23547f6f4637e52457658b43192ae344256
Alperen <aalperenelbasan@gmail.com>
2021-03-17 12:34:20 -0500

    adminHomePage changes

--- src/main/java/com/mumSchud/springbootmum/controllers/PagesController.java
+++ src/main/java/com/mumSchud/springbootmum/controllers/PagesController.java
@@ -1,17 +1,21 @@
 package com.mumSchud.springbootmum.controllers;
 
+import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PagesController {
 	@RequestMapping("/")
 	public String homePage() {
-    	  return "index";	
-}
+		return "index";
+	}
 
-@RequestMapping("/admin")
-public String adminPage() {
-	  return "adminHomePage";	}
+	@RequestMapping("/admin")
+	public String adminPage(Model model, Principal principal) {
+		model.addAttribute("authUserName", principal.getName());
+		return "adminHomePage";
+	}
 }
